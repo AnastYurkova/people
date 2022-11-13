d65526e0efcb504b943357b037d5bccc34ea6dda
AnastYurkova <anastasia.yurkova2003@mail.ru>
2022-11-13 14:33:35 +0300

    fixed bugs

--- src/main/java/com/ku/people/UserRepository.java
+++ src/main/java/com/ku/people/UserRepository.java
@@ -18,20 +18,24 @@ public class UserRepository {
     public static final String ROLE_NAME_COLUMN = "role_name";
     public static final String DETAILS_ID_COLUMN = "detail_id";
 
-    public static final String QUERY_FIND_BY_ID = """
-                SELECT u.id, u.user_name, u.password, u.surname, u.name, r.id role_id, r.role_name, d.id detail_id
-                FROM users u
-                    LEFT JOIN user_role_links url  ON u.id = url.user_id
-                    LEFT JOIN roles r ON url.role_id = r.id
-                    LEFT JOIN details d ON d.user_id = u.id 
-                WHERE u.id = ?
-            """;
-    public static final String QUERY_FIND_ALL = "SELECT * FROM users";
-    public static final String QUERY_SAVE = "INSERT INTO users(user_name, password, surname, name) VALUES (?, ?, ?, ?)";
-    public static final String QUERY_UPDATE = "UPDATE users SET user_name = ?, password = ?, surname = ?, name = ? WHERE id = ?";
-    public static final String QUERY_DELETE_DETAILS = "DELETE FROM details WHERE user_id = ? ";
-    public static final String QUERY_DELETE_USER_ROLE_LINKS = "DELETE FROM user_role_links WHERE user_id = ? ";
-    public static final String QUERY_DELETE = "DELETE FROM users WHERE id = ? ";
+    public static final String FIND_BY_ID_QUERY = """
+        SELECT u.id, u.user_name, u.password, u.surname, u.name, r.id role_id, r.role_name, d.id detail_id
+        FROM users u
+            LEFT JOIN user_role_links url  ON u.id = url.user_id
+            LEFT JOIN roles r ON url.role_id = r.id
+            LEFT JOIN details d ON d.user_id = u.id 
+        WHERE u.id = ?
+    """;
+    public static final String FIND_ALL_QUERY = "SELECT * FROM users";
+    public static final String SAVE_QUERY = """
+        INSERT INTO users(user_name, password, surname, name) VALUES (?, ?, ?, ?)
+    """;
+    public static final String UPDATE_QUERY = """
+        UPDATE users SET user_name = ?, password = ?, surname = ?, name = ? WHERE id = ?
+    """;
+    public static final String DELETE_DETAILS_QUERY = "DELETE FROM details WHERE user_id = ? ";
+    public static final String DELETE_USER_ROLE_LINKS_QUERY = "DELETE FROM user_role_links WHERE user_id = ? ";
+    public static final String DELETE_QUERY = "DELETE FROM users WHERE id = ? ";
 
     private final DataSource dataSource;
 
@@ -42,12 +46,24 @@ public class UserRepository {
     public User findById(Long id) {
         User user = new User();
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(QUERY_FIND_BY_ID)
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID_QUERY)
         ) {
             preparedStatement.setLong(1, id);
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 while (resultSet.next()) {
+                    List<Role> roles = new ArrayList<>();
+                    List<Detail> details = new ArrayList<>();
                     fillUser(user, resultSet);
+                    do {
+                        if (!roles.contains(buildRole(resultSet))) {
+                            roles.add(buildRole(resultSet));
+                        }
+                        if (!details.contains(buildDetails(resultSet))) {
+                            details.add(buildDetails(resultSet));
+                        }
+                    } while (resultSet.next());
+                    user.setRoles(roles);
+                    user.setDetails(details);
                 }
             }
         } catch (Exception ex) {
@@ -58,23 +74,11 @@ public class UserRepository {
 
     private void fillUser(User user, ResultSet resultSet) {
         try {
-            List<Role> roles = new ArrayList<>();
-            List<Detail> details = new ArrayList<>();
             user.setId(resultSet.getLong(ID_COLUMN));
             user.setUsername(resultSet.getString(USER_NAME_COLUMN));
             user.setPassword(resultSet.getString(PASSWORD_COLUMN));
             user.setSurname(resultSet.getString(SURNAME_COLUMN));
             user.setName(resultSet.getString(NAME_COLUMN));
-            do {
-                if (!roles.contains(buildRole(resultSet))) {
-                    roles.add(buildRole(resultSet));
-                }
-                if (!details.contains(buildDetails(resultSet))) {
-                    details.add(buildDetails(resultSet));
-                }
-            } while (resultSet.next());
-            user.setRoles(roles);
-            user.setDetails(details);
         } catch (Exception ex) {
             throw new UserException("Failed to fill user", ex);
         }
@@ -101,17 +105,11 @@ public class UserRepository {
         List<User> users = new ArrayList<>();
         try (Connection connection = dataSource.getConnection();
              Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(QUERY_FIND_ALL);
+             ResultSet resultSet = statement.executeQuery(FIND_ALL_QUERY);
         ) {
             while (resultSet.next()) {
                 User user = new User();
-                user.setRoles(new ArrayList<>());
-                user.setDetails(new ArrayList<>());
-                user.setId(resultSet.getLong(ID_COLUMN));
-                user.setUsername(resultSet.getString(USER_NAME_COLUMN));
-                user.setPassword(resultSet.getString(PASSWORD_COLUMN));
-                user.setSurname(resultSet.getString(SURNAME_COLUMN));
-                user.setName(resultSet.getString(NAME_COLUMN));
+                fillUser(user, resultSet);
                 users.add(user);
             }
         } catch (Exception ex) {
@@ -120,59 +118,47 @@ public class UserRepository {
         return users;
     }
 
-    public boolean save(User user) throws Exception {
+    public void save(User user) throws Exception {
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(QUERY_SAVE, Statement.RETURN_GENERATED_KEYS)
+             PreparedStatement preparedStatement = connection.prepareStatement(SAVE_QUERY)
         ) {
             try {
-                setUser(preparedStatement, user);
+                buildQuery(preparedStatement, user);
                 preparedStatement.executeUpdate();
             } catch (Exception ex) {
                 throw new UserException("Failed to save user", ex);
             }
         }
-        return true;
     }
 
-    private void setUser(PreparedStatement preparedStatement, User user) {
+    private void buildQuery(PreparedStatement preparedStatement, User user) {
         try {
             preparedStatement.setString(1, user.getUsername());
             preparedStatement.setString(2, user.getPassword());
             preparedStatement.setString(3, user.getSurname());
             preparedStatement.setString(4, user.getName());
         } catch (Exception ex) {
-            throw new UserException("Failed to save user", ex);
+            throw new UserException("Failed to put user", ex);
         }
     }
 
-    public boolean update(User user) throws Exception {
+    public void update(User user) throws Exception {
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(QUERY_UPDATE);
+             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_QUERY);
         ) {
             try {
-                connection.setAutoCommit(false);
-
-                preparedStatement.setString(1, user.getUsername());
-                preparedStatement.setString(2, user.getPassword());
-                preparedStatement.setString(3, user.getSurname());
-                preparedStatement.setString(4, user.getName());
+                buildQuery(preparedStatement, user);
                 preparedStatement.setLong(5, user.getId());
                 preparedStatement.executeUpdate();
-
-                connection.commit();
             } catch (Exception ex) {
-                connection.rollback();
                 throw new UserException("Failed to delete user", ex);
-            } finally {
-                connection.setAutoCommit(true);
             }
         }
-        return true;
     }
 
-    public boolean delete(Long id) throws Exception {
+    public void delete(Long id) throws Exception {
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(QUERY_DELETE);
+             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_QUERY);
         ) {
             try {
                 connection.setAutoCommit(false);
@@ -188,11 +174,10 @@ public class UserRepository {
                 connection.setAutoCommit(true);
             }
         }
-        return true;
     }
 
     private void deleteUserRoleLink(Connection connection, Long id) throws Exception {
-        try (PreparedStatement preparedStatementForRoles = connection.prepareStatement(QUERY_DELETE_USER_ROLE_LINKS)
+        try (PreparedStatement preparedStatementForRoles = connection.prepareStatement(DELETE_USER_ROLE_LINKS_QUERY)
         ) {
             try {
                 preparedStatementForRoles.setLong(1, id);
@@ -205,7 +190,7 @@ public class UserRepository {
     }
 
     private void deleteDetails(Connection connection, Long id) throws Exception {
-        try (PreparedStatement preparedStatementForDetails = connection.prepareStatement(QUERY_DELETE_DETAILS)
+        try (PreparedStatement preparedStatementForDetails = connection.prepareStatement(DELETE_DETAILS_QUERY)
         ) {
             try {
                 preparedStatementForDetails.setLong(1, id);
@@ -216,6 +201,5 @@ public class UserRepository {
             }
         }
     }
-
 }
 
