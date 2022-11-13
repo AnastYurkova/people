49df06b42e83488e5dc22780a126bc4becd94cce
AnastYurkova <anastasia.yurkova2003@mail.ru>
2022-11-13 15:15:47 +0300

    fixed bugs

--- src/main/java/com/ku/people/Repository.java
+++ src/main/java/com/ku/people/Repository.java
@@ -11,20 +11,20 @@ public interface Repository {
     String DETAIL_ID_COLUMN = "detail_id";
 
     String FIND_BY_ID_QUERY = """
-                SELECT u.id, u.user_name, u.password, u.surname, u.name, r.id role_id, r.role_name, d.id detail_id
-                FROM users u
-                    LEFT JOIN user_role_links url  ON u.id = url.user_id
-                    LEFT JOIN roles r ON url.role_id = r.id
-                    LEFT JOIN details d ON d.user_id = u.id 
-                WHERE u.id = ?
-            """;
+        SELECT u.id, u.user_name, u.password, u.surname, u.name, r.id role_id, r.role_name, d.id detail_id
+        FROM users u
+            LEFT JOIN user_role_links url  ON u.id = url.user_id
+            LEFT JOIN roles r ON url.role_id = r.id
+            LEFT JOIN details d ON d.user_id = u.id 
+        WHERE u.id = ?
+    """;
     String FIND_ALL_QUERY = "SELECT * FROM users";
     String SAVE_QUERY = """
-                INSERT INTO users(user_name, password, surname, name) VALUES (?, ?, ?, ?)
-            """;
+         INSERT INTO users(user_name, password, surname, name) VALUES (?, ?, ?, ?)
+    """;
     String UPDATE_QUERY = """
-                UPDATE users SET user_name = ?, password = ?, surname = ?, name = ? WHERE id = ?
-            """;
+         UPDATE users SET user_name = ?, password = ?, surname = ?, name = ? WHERE id = ?
+    """;
     String DELETE_DETAIL_QUERY = "DELETE FROM details WHERE user_id = ?";
     String DELETE_USER_ROLE_LINKS_QUERY = "DELETE FROM user_role_links WHERE user_id = ?";
     String DELETE_QUERY = "DELETE FROM users WHERE id = ?";
