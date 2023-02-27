a585388fa6b3670c63c03c75d02eb194033ef855
AnastYurkova <anastasia.yurkova2003@mail.ru>
2023-02-27 15:32:18 +0300

    change Repository

--- src/main/java/com/ku/people/repository/DetailRepository.java
+++ src/main/java/com/ku/people/repository/DetailRepository.java
@@ -1,10 +1,19 @@
 package com.ku.people.repository;
 
 import com.ku.people.entity.Detail;
+import com.ku.people.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface DetailRepository extends JpaRepository<Detail, Long> {
+    @Query("FROM Detail d LEFT JOIN FETCH d.user LEFT JOIN FETCH d.relationship WHERE d.id = :id")
+    Optional<Detail> findById(Long id);
 
+//    @Query("INSERT INTO details(relationship_type, user_id, relationship_id) " +
+//            "VALUES (?\\\\:\\\\:relationship_type_enum, ?, ?)")
+//    Optional<Detail> save();
 }
